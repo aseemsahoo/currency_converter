45df4f5b6490c7aadca8ab4da9dcee9ae061df28
aseemsahoo <aseemsahoo.v@gmail.com>
2022-09-20 19:03:05 +0530

    Full realtime API functioning

--- currency_converter/home.Designer.cs
+++ currency_converter/home.Designer.cs
@@ -38,7 +38,6 @@
             this.converted_label = new System.Windows.Forms.Label();
             this.convert_button = new Guna.UI2.WinForms.Guna2Button();
             this.reset_button = new Guna.UI2.WinForms.Guna2Button();
-            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
             this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
             this.SuspendLayout();
@@ -58,13 +57,13 @@
             this.from_textbox.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.from_textbox.ForeColor = System.Drawing.Color.Black;
             this.from_textbox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.from_textbox.Location = new System.Drawing.Point(136, 126);
-            this.from_textbox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.from_textbox.Location = new System.Drawing.Point(109, 101);
+            this.from_textbox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.from_textbox.Name = "from_textbox";
             this.from_textbox.PasswordChar = '\0';
             this.from_textbox.PlaceholderText = "";
             this.from_textbox.SelectedText = "";
-            this.from_textbox.Size = new System.Drawing.Size(210, 58);
+            this.from_textbox.Size = new System.Drawing.Size(168, 46);
             this.from_textbox.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
             this.from_textbox.TabIndex = 0;
             // 
@@ -83,73 +82,73 @@
             this.to_textbox.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.to_textbox.ForeColor = System.Drawing.Color.Black;
             this.to_textbox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.to_textbox.Location = new System.Drawing.Point(136, 222);
-            this.to_textbox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.to_textbox.Location = new System.Drawing.Point(109, 178);
+            this.to_textbox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.to_textbox.Name = "to_textbox";
             this.to_textbox.PasswordChar = '\0';
             this.to_textbox.PlaceholderText = "";
             this.to_textbox.ReadOnly = true;
             this.to_textbox.SelectedText = "";
-            this.to_textbox.Size = new System.Drawing.Size(210, 58);
+            this.to_textbox.Size = new System.Drawing.Size(168, 46);
             this.to_textbox.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
             this.to_textbox.TabIndex = 1;
             // 
             // to_comboBox
             // 
             this.to_comboBox.BackColor = System.Drawing.Color.Transparent;
+            this.to_comboBox.BorderColor = System.Drawing.Color.DodgerBlue;
             this.to_comboBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.to_comboBox.DropDownHeight = 200;
             this.to_comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.to_comboBox.DropDownWidth = 50;
             this.to_comboBox.FillColor = System.Drawing.Color.DodgerBlue;
             this.to_comboBox.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.to_comboBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.to_comboBox.FocusedState.FillColor = System.Drawing.Color.Transparent;
-            this.to_comboBox.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.to_comboBox.ForeColor = System.Drawing.Color.White;
+            this.to_comboBox.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.to_comboBox.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.to_comboBox.ItemHeight = 51;
-            this.to_comboBox.Items.AddRange(new object[] {
-            "USD",
-            "INR",
-            "EUR"});
-            this.to_comboBox.Location = new System.Drawing.Point(352, 222);
-            this.to_comboBox.MaxDropDownItems = 10;
+            this.to_comboBox.Location = new System.Drawing.Point(282, 178);
+            this.to_comboBox.Margin = new System.Windows.Forms.Padding(2);
+            this.to_comboBox.MaxDropDownItems = 20;
             this.to_comboBox.Name = "to_comboBox";
-            this.to_comboBox.Size = new System.Drawing.Size(88, 57);
+            this.to_comboBox.Size = new System.Drawing.Size(85, 57);
             this.to_comboBox.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
             this.to_comboBox.TabIndex = 2;
             // 
             // from_comboBox
             // 
             this.from_comboBox.BackColor = System.Drawing.Color.Transparent;
+            this.from_comboBox.BorderColor = System.Drawing.Color.DodgerBlue;
             this.from_comboBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.from_comboBox.DropDownHeight = 200;
             this.from_comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.from_comboBox.DropDownWidth = 50;
             this.from_comboBox.FillColor = System.Drawing.Color.DodgerBlue;
             this.from_comboBox.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.from_comboBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.from_comboBox.FocusedState.FillColor = System.Drawing.Color.Transparent;
-            this.from_comboBox.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.from_comboBox.ForeColor = System.Drawing.Color.White;
+            this.from_comboBox.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.from_comboBox.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.from_comboBox.FormattingEnabled = true;
             this.from_comboBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.from_comboBox.ItemHeight = 51;
-            this.from_comboBox.Items.AddRange(new object[] {
-            "USD",
-            "INR",
-            "EUR"});
-            this.from_comboBox.Location = new System.Drawing.Point(352, 126);
-            this.from_comboBox.MaxDropDownItems = 10;
+            this.from_comboBox.Location = new System.Drawing.Point(282, 101);
+            this.from_comboBox.Margin = new System.Windows.Forms.Padding(2);
+            this.from_comboBox.MaxDropDownItems = 20;
             this.from_comboBox.Name = "from_comboBox";
-            this.from_comboBox.Size = new System.Drawing.Size(88, 57);
+            this.from_comboBox.Size = new System.Drawing.Size(85, 57);
             this.from_comboBox.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
             this.from_comboBox.TabIndex = 3;
-            this.from_comboBox.SelectedIndexChanged += new System.EventHandler(this.from_comboBox_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(126, 20);
+            this.label1.Location = new System.Drawing.Point(101, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(320, 46);
+            this.label1.Size = new System.Drawing.Size(256, 37);
             this.label1.TabIndex = 4;
             this.label1.Text = "Currency Converter";
             // 
@@ -157,9 +156,10 @@
             // 
             this.amount_label.AutoSize = true;
             this.amount_label.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.amount_label.Location = new System.Drawing.Point(132, 103);
+            this.amount_label.Location = new System.Drawing.Point(106, 82);
+            this.amount_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.amount_label.Name = "amount_label";
-            this.amount_label.Size = new System.Drawing.Size(72, 23);
+            this.amount_label.Size = new System.Drawing.Size(59, 19);
             this.amount_label.TabIndex = 5;
             this.amount_label.Text = "Amount";
             // 
@@ -167,9 +167,10 @@
             // 
             this.converted_label.AutoSize = true;
             this.converted_label.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.converted_label.Location = new System.Drawing.Point(132, 199);
+            this.converted_label.Location = new System.Drawing.Point(106, 159);
+            this.converted_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.converted_label.Name = "converted_label";
-            this.converted_label.Size = new System.Drawing.Size(110, 23);
+            this.converted_label.Size = new System.Drawing.Size(90, 19);
             this.converted_label.TabIndex = 6;
             this.converted_label.Text = "Converted to";
             // 
@@ -185,9 +186,10 @@
             this.convert_button.FillColor = System.Drawing.Color.Gainsboro;
             this.convert_button.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.convert_button.ForeColor = System.Drawing.Color.Navy;
-            this.convert_button.Location = new System.Drawing.Point(134, 323);
+            this.convert_button.Location = new System.Drawing.Point(107, 258);
+            this.convert_button.Margin = new System.Windows.Forms.Padding(2);
             this.convert_button.Name = "convert_button";
-            this.convert_button.Size = new System.Drawing.Size(128, 39);
+            this.convert_button.Size = new System.Drawing.Size(102, 31);
             this.convert_button.TabIndex = 7;
             this.convert_button.Text = "Convert";
             this.convert_button.Click += new System.EventHandler(this.convert_button_Click);
@@ -204,31 +206,13 @@
             this.reset_button.FillColor = System.Drawing.Color.Gainsboro;
             this.reset_button.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.reset_button.ForeColor = System.Drawing.Color.Navy;
-            this.reset_button.Location = new System.Drawing.Point(310, 323);
+            this.reset_button.Location = new System.Drawing.Point(248, 258);
+            this.reset_button.Margin = new System.Windows.Forms.Padding(2);
             this.reset_button.Name = "reset_button";
-            this.reset_button.Size = new System.Drawing.Size(128, 39);
+            this.reset_button.Size = new System.Drawing.Size(102, 31);
             this.reset_button.TabIndex = 8;
             this.reset_button.Text = "Reset";
             this.reset_button.Click += new System.EventHandler(this.reset_button_Click);
-            // 
-            // guna2Button1
-            // 
-            this.guna2Button1.BorderColor = System.Drawing.Color.Navy;
-            this.guna2Button1.BorderRadius = 8;
-            this.guna2Button1.BorderThickness = 2;
-            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.guna2Button1.FillColor = System.Drawing.Color.Gainsboro;
-            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2Button1.ForeColor = System.Drawing.Color.Navy;
-            this.guna2Button1.Location = new System.Drawing.Point(136, 323);
-            this.guna2Button1.Name = "guna2Button1";
-            this.guna2Button1.Size = new System.Drawing.Size(128, 39);
-            this.guna2Button1.TabIndex = 7;
-            this.guna2Button1.Text = "Convert";
-            this.guna2Button1.Click += new System.EventHandler(this.convert_button_Click);
             // 
             // errorProvider
             // 
@@ -236,12 +220,11 @@
             // 
             // mainwindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.Gainsboro;
-            this.ClientSize = new System.Drawing.Size(572, 408);
+            this.ClientSize = new System.Drawing.Size(458, 326);
             this.Controls.Add(this.reset_button);
-            this.Controls.Add(this.guna2Button1);
             this.Controls.Add(this.convert_button);
             this.Controls.Add(this.converted_label);
             this.Controls.Add(this.amount_label);
@@ -251,6 +234,7 @@
             this.Controls.Add(this.to_textbox);
             this.Controls.Add(this.from_textbox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "mainwindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -272,7 +256,6 @@
         private System.Windows.Forms.Label converted_label;
         private Guna.UI2.WinForms.Guna2Button convert_button;
         private Guna.UI2.WinForms.Guna2Button reset_button;
-        private Guna.UI2.WinForms.Guna2Button guna2Button1;
         private System.Windows.Forms.ErrorProvider errorProvider;
     }
 }
--- currency_converter/home.cs
+++ currency_converter/home.cs
@@ -7,6 +7,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -17,14 +18,19 @@ namespace currency_converter
 {
     public partial class mainwindow : Form
     {
-        //double[,] curr = new double[3, 3]
+        //double[,] rates = new double[3, 3]
         //{
         //    {1, 79.5944, 1.00133},
         //    {0.01254, 1, 0.01256},
         //    {0.99851, 79.4757,1}
         //};
+        double[,] rates = new double[15, 15];
 
-
+        List<string> currencies = new List<string>(new string[]
+        {
+                "USD", "EUR", "INR", "JPY", "AUD", "GBP", "CAD", "CHF",
+                "CNY", "HKD", "NZD", "SEK", "SGD", "MXN", "RUB"
+        });
         public mainwindow()
         {
             InitializeComponent();
@@ -32,7 +38,7 @@ namespace currency_converter
             vector<string> curr 
             {
 	            "USD", "EUR", "JPY", "AUD", "GBP", "CAD", "CHF", "CNY", 
-	            "HKD", "NZD", "SEK", "SGD", "NOK", "MXN", "INR", "RUB"
+	            "HKD", "NZD", "SEK", "SGD", "MXN", "INR", "RUB"
             };
             for (int i = 0; i < 20; i++)
             {
@@ -48,29 +54,44 @@ namespace currency_converter
                 }
             }
             */
-            fill_data();
 
+            foreach (string s in currencies)
+            {
+                from_comboBox.Items.Add(s);
+                to_comboBox.Items.Add(s);
+            }
+            fill_data();
+        }
+        private void update_currencies(int i, Currencies Base)
+        {
+            rates[i, 0] = Base.usd;
+            rates[i, 1] = Base.eur;
+            rates[i, 2] = Base.inr;
+            rates[i, 3] = Base.jpy;
+            rates[i, 4] = Base.aud;
+            rates[i, 5] = Base.gbp;
+            rates[i, 6] = Base.cad;
+            rates[i, 7] = Base.chf;
+            rates[i, 8] = Base.cny;
+            rates[i, 9] = Base.hkd;
+            rates[i, 10] = Base.nzd;
+            rates[i, 11] = Base.sek;
+            rates[i, 12] = Base.sgd;
+            rates[i, 13] = Base.mxn;
+            rates[i, 14] = Base.rub;
         }
 
         private async void fill_data()
         {
-            List<string> curr = new List<string>(new string[]
-            {
-                "USD", "EUR", "JPY", "AUD", "GBP", "CAD", "CHF", "CNY",
-                "HKD", "NZD", "SEK", "SGD", "MXN", "INR", "RUB"
-            });
             for (int i = 0; i < 15; i++)
             {
-                string base_currency = curr[i].ToLower();
+                string base_currency = currencies[i].ToLower();
                 string url = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/" + base_currency + ".json";
-                Root output = await url.GetJsonAsync<Root>();
 
-                //var a = output.Bas;
-                //var base_c = output.Base;
-                //var to = base_c.inr;
+                Root output = await url.GetJsonAsync<Root>();
+                update_currencies(i, output.Base);
             }
         }
-
         private void reset_button_Click(object sender, EventArgs e)
         {
             from_textbox.Clear();
@@ -80,15 +101,13 @@ namespace currency_converter
         private bool validate_amount()
         {
             errorProvider.Clear();
-         
-         
             double test;
-            if(double.TryParse(from_textbox.Text, out test) == false)
+            if (double.TryParse(from_textbox.Text, out test) == false)
             {
                 errorProvider.SetError(from_textbox, "Please enter an amount");
                 return false;
             }
-             if (from_comboBox.SelectedIndex == -1)
+            if (from_comboBox.SelectedIndex == -1)
             {
                 errorProvider.SetError(from_comboBox, "Please select a currency");
                 return false;
@@ -107,18 +126,14 @@ namespace currency_converter
             {
                 return;
             }
-          
             double input;
             double.TryParse(from_textbox.Text, out input);
+
             int i = from_comboBox.SelectedIndex;
             int j = to_comboBox.SelectedIndex;
-            // double res = curr[i, j] * input;
-            // to_textbox.Text = res.ToString();
-        }
 
-        private void from_comboBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
+            double res = rates[i, j] * input;
+            to_textbox.Text = res.ToString();
         }
     }
 }
