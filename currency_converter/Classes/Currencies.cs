44c954db79dc52649d8d8f6331c88aea32a67369
cat <cat@LAPTOP-CPRL6L7O>
2022-09-20 14:01:23 +0530

    Added API fetching classes

---
+++ currency_converter/Classes/Currencies.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currency_converter.Classes
+{
+    public class Currencies
+    {
+        [JsonProperty("aud")]
+        public double aud { get; set; }
+
+        [JsonProperty("cad")]
+        public double cad { get; set; }
+
+        [JsonProperty("chf")]
+        public double chf { get; set; }
+
+        [JsonProperty("cny")]
+        public double cny { get; set; }
+
+        [JsonProperty("eur")]
+        public double eur { get; set; }
+
+        [JsonProperty("gbp")]
+        public double gbp { get; set; }
+
+        [JsonProperty("hkd")]
+        public double hkd { get; set; }
+
+        [JsonProperty("inr")]
+        public double inr { get; set; }
+
+        [JsonProperty("jpy")]
+        public double jpy { get; set; }
+
+        [JsonProperty("mxn")]
+        public double mxn { get; set; }
+
+        [JsonProperty("nzd")]
+        public double nzd { get; set; }
+
+        [JsonProperty("rub")]
+        public double rub { get; set; }
+
+        [JsonProperty("sek")]
+        public double sek { get; set; }
+
+        [JsonProperty("sgd")]
+        public double sgd { get; set; }
+
+        [JsonProperty("usd")]
+        public double usd { get; set; }
+    }
+
+    public class Root
+    {
+        // using json property because different name of object compared to JSON name in website result
+        [JsonProperty("date")]
+        public string Date { get; set; }
+
+        public Currencies Base { get; set; }
+
+        //public Currencies aud_base { set { Base = value; } }
+        [JsonProperty("aud")]
+        private Currencies aud_base { set { Base = value; } }
+
+        [JsonProperty("cad")]
+        private Currencies cad_base { set { Base = value; } }
+
+        [JsonProperty("chf")]
+        private Currencies chf_base { set { Base = value; } }
+
+        [JsonProperty("cny")]
+        private Currencies cny_base { set { Base = value; } }
+
+        [JsonProperty("eur")]
+        private Currencies eur_base { set { Base = value; } }
+
+        [JsonProperty("gbp")]
+        private Currencies gbp_base { set { Base = value; } }
+
+        [JsonProperty("hkd")]
+        private Currencies hkd_base { set { Base = value; } }
+
+        [JsonProperty("inr")]
+        private Currencies inr_base { set { Base = value; } }
+
+        [JsonProperty("jpy")]
+        private Currencies jpy_base { set { Base = value; } }
+
+        [JsonProperty("mxn")]
+        private Currencies mxn_base { set { Base = value; } }
+
+        [JsonProperty("nzd")]
+        private Currencies nzd_base { set { Base = value; } }
+
+        [JsonProperty("rub")]
+        private Currencies rub_base { set { Base = value; } }
+
+        [JsonProperty("sek")]
+        private Currencies sek_base { set { Base = value; } }
+
+        [JsonProperty("sgd")]
+        private Currencies sgd_base { set { Base = value; } }
+
+        [JsonProperty("usd")]
+        private Currencies usd_base { set { Base = value; } }
+    }
+}
--- currency_converter/home.cs
+++ currency_converter/home.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using currency_converter.Classes;
+using Flurl.Http;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -14,24 +17,64 @@ namespace currency_converter
 {
     public partial class mainwindow : Form
     {
-        double[,] curr = new double[3, 3]
-        {
-            {1, 79.5944, 1.00133},
-            {0.01254, 1, 0.01256},
-            {0.99851, 79.4757,1}
-        };
+        //double[,] curr = new double[3, 3]
+        //{
+        //    {1, 79.5944, 1.00133},
+        //    {0.01254, 1, 0.01256},
+        //    {0.99851, 79.4757,1}
+        //};
+
 
         public mainwindow()
         {
             InitializeComponent();
+            /*
+            vector<string> curr 
+            {
+	            "USD", "EUR", "JPY", "AUD", "GBP", "CAD", "CHF", "CNY", 
+	            "HKD", "NZD", "SEK", "SGD", "NOK", "MXN", "INR", "RUB"
+            };
+            for (int i = 0; i < 20; i++)
+            {
+                string base = curr[i];
+                string url "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/" + base + ".json";
+                //al the a adat wutg there string are fetched 
+                unordered_map<string, double> all_data = fetch(url);
+                //only 20 valid currencues are taken here 
+                for (int j = 0; j < 20; j++)
+                {
+                    string dest = curr[j];
+                    mat[i][j] = all_data[dest];
+                }
+            }
+            */
+            fill_data();
+
+        }
+
+        private async void fill_data()
+        {
+            List<string> curr = new List<string>(new string[]
+            {
+                "USD", "EUR", "JPY", "AUD", "GBP", "CAD", "CHF", "CNY",
+                "HKD", "NZD", "SEK", "SGD", "MXN", "INR", "RUB"
+            });
+            for (int i = 0; i < 15; i++)
+            {
+                string base_currency = curr[i].ToLower();
+                string url = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/" + base_currency + ".json";
+                Root output = await url.GetJsonAsync<Root>();
+
+                //var a = output.Bas;
+                //var base_c = output.Base;
+                //var to = base_c.inr;
+            }
         }
 
         private void reset_button_Click(object sender, EventArgs e)
         {
             from_textbox.Clear();
             to_textbox.Clear();
-            //from_comboBox.SelectedIndex = -1;
-            //to_comboBox.SelectedIndex = -1;
         }
 
         private bool validate_amount()
@@ -69,8 +112,8 @@ namespace currency_converter
             double.TryParse(from_textbox.Text, out input);
             int i = from_comboBox.SelectedIndex;
             int j = to_comboBox.SelectedIndex;
-            double res = curr[i, j] * input;
-            to_textbox.Text = res.ToString();
+            // double res = curr[i, j] * input;
+            // to_textbox.Text = res.ToString();
         }
 
         private void from_comboBox_SelectedIndexChanged(object sender, EventArgs e)
